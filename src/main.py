a00b278e6ec2cd80723d9e929725fb89d71c24f9
Felipe Araujo <felipe.ar@pm.me>
2022-06-05 21:10:51 -0300

    v1.0

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-import os
-from features.camera.camera import Camera
 if __name__ == "__main__":
-    dir_home = os.path.dirname(os.path.realpath(__file__))
-    Camera(dir_home).start()
+    print("HELLO WORLD")
 
